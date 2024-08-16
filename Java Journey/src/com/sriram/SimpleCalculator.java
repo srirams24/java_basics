2838c9fd775030348024723a3d45057204d53ad3
Sriram S <124426883+srirams24@users.noreply.github.com>
2024-08-16 03:53:54 +0000

    Fri, Aug 16, 2024, 9:23 AM +05:30

--- Java Journey/src/com/sriram/SimpleCalculator.java
+++ Java Journey/src/com/sriram/SimpleCalculator.java
@@ -5,7 +5,7 @@ package com.sriram;
 public class SimpleCalculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World);
+        System.out.println("Hello World");
     }
 
 }
